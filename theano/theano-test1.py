9b014c408ccc4be7ecafec2ccfa9b50c95910f24
Daniel Speck <daniel@dspeck.de>
2015-08-11 17:17:03 +0200

    baby steps

--- theano/theano-test1.py
+++ theano/theano-test1.py
@@ -2,4 +2,13 @@ __author__ = 'daniel'
 
 
 from theano import tensor as T
+from theano.tensor.shared_randomstreams import RandomStreams
 from theano import function
+
+# random stream object
+srng = RandomStreams(seed=234)
+
+# random uniform distribution
+rnd_uniform = srng.uniform((2, 2))           # random uniform object
+get_rnd_uniform = function([], rnd_uniform)  # getter function
+
